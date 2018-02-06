623f5f2d7975d71779cebe7db77f516d720bca41
Austin Dudas <dudasaus@umich.edu>
2018-02-06 10:55:16 -0500

    Added comments model

--- app/routes/comments.js
+++ app/routes/comments.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [{
+      id: 0,
+      user: 'Austin',
+      text: 'This is a comment!'
+    }, {
+      id: 1,
+      user: 'Bob',
+      text: 'This is another comment!'
+    }];
+  }
 });
